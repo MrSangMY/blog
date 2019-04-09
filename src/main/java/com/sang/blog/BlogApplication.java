3c8cb9e4bc69f4a0cd491ac5e4ec4bd143dfaa40
sang <2431517621@qq.com>
2019-04-09 15:17:51 +0800

    新建blog项目

--- src/main/java/com/sang/blog/BlogApplication.java
+++ src/main/java/com/sang/blog/BlogApplication.java
@@ -1,14 +1,23 @@
 package com.sang.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
-        System.out.println();
+        System.out.println("启动成功》》》》》》》》");
+        System.out.println("启动成功》》》》》》》》");
+        System.out.println("启动成功》》》》》》》》");
+        System.out.println("启动成功》》》》》》》》");
+        System.out.println("启动成功》》》》》》》》");
+        System.out.println("启动成功》》》》》》》》");
     }
 
 }
---
+++ src/main/webapp/WEB-INF/view/test.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:set var="path" value="${pageContext.request.contextPath }"/>
+<html>
+<head>
+</head>
+<body>
+    <h1>BBBBBBBBBBBBBBBBBBBBBB</h1>
+</body>
+
+
+<jsp:include page="/WEB-INF/view/base/baseJS.jsp"/>
+</html>
